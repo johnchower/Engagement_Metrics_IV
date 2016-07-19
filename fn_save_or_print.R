d88f0aa9bc5e7b6968c810da4d5342981a1b91a6
johnchower <johnchower@gmail.com>
2016-07-19 17:10:45 -0600

    Allow resizing of browser window, and allow pdf outputs.

--- fn_save_or_print.R
+++ fn_save_or_print.R
@@ -8,6 +8,8 @@ save_or_print <-
     , plot_name = 
         paste("myplot", gsub(" ", "_",Sys.time()), sep = "_")
     , outformat = "html"
+    , v.width = 992
+    , v.height = 744
   ){
     current_wd <- getwd()
     if(save_plots){
@@ -16,13 +18,25 @@ save_or_print <-
         saveWidget(as.widget(p)
                    , paste(plot_name, ".html", sep = "")
         ) 
-      } else{
+      } else if(outformat == "png"){
         saveWidget(as.widget(p)
                    , paste(plot_name, ".html", sep = "")
         )
         webshot(
           paste(plot_name, ".html", sep = "")
           , file = paste(plot_name, ".png", sep = "")
+          , vwidth = v.width
+          , vheight = v.height
+        )
+      } else if(outformat == "pdf"){
+        saveWidget(as.widget(p)
+                   , paste(plot_name, ".html", sep = "")
+        )
+        webshot(
+          paste(plot_name, ".html", sep = "")
+          , file = paste(plot_name, ".pdf", sep = "")
+          , vwidth = v.width
+          , vheight = v.height
         )
       }
       setwd(current_wd)

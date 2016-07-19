64954de3f8e072090d07a19f556e9aad8924177e
johnchower <johnchower@gmail.com>
2016-07-19 12:16:50 -0600

    Calculate user breakdown by current state.

--- ms_key_metrics.R
+++ ms_key_metrics.R
@@ -64,3 +64,33 @@ core_user_by_actual_actions <- date_user_table %>%
 
 # User states
 
+user_breakdown_by_current_state_data <- user_facts %>%
+  filter(user_id %in% end_users) %>%
+  group_by(current_segment) %>%
+  summarise(number_of_users = n()) %>%
+  mutate(
+    total_number_of_users = sum(number_of_users)
+    , percent_of_users = number_of_users/total_number_of_users
+  ) %>% 
+  arrange(desc(percent_of_users))
+
+user_breakdown_by_current_state_data %>%
+  mutate(percent_of_users = round(percent_of_users, 3)) %>%
+  plot_ly(
+    type = "bar"
+    , x = current_segment
+    , y = percent_of_users
+    , text = paste(100*percent_of_users, "% (", as.character(number_of_users), " users)", sep = "")
+    , mode = "text"
+    , hoverinfo = "text"
+  ) %>%
+  bar_chart_layout(
+    charttitle = 
+      paste(
+        "Percent of Users in Each State ("
+        , user_breakdown_by_current_state_data$total_number_of_users[1]
+        , " Total Users)"
+        , sep = ""
+      ) 
+    , yaxisformat = "%"
+  )

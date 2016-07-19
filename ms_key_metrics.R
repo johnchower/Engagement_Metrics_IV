3accad29b70d7927a4edeec11c60726431fe18be
johnchower <johnchower@gmail.com>
2016-07-19 14:53:08 -0600

    Colorize bar chart and add google sheets API interaction.

--- ms_key_metrics.R
+++ ms_key_metrics.R
@@ -1,6 +1,11 @@
 # Exploration: Cornerstone metrics
 
 source("fn_moving_sum.r")
+source("fn_bar_chart_layout.r")
+library(googlesheets)
+library(dplyr)
+library(plyr)
+library(plotly)
 
 date_user_table <- 
   read.table(
@@ -20,6 +25,9 @@ user_facts <-
   ) %>%
   mutate(date = as.Date(date,format="%Y-%m-%d"))
 
+core_user_metrics_oldgs <- gs_title("core_user_metrics") %>% 
+  gs_read
+
 # Subsets
 
 end_users <- user_facts %>%
@@ -28,6 +36,9 @@ end_users <- user_facts %>%
   unique
 
 # Number of core users (defined monthly) who have showed up in past 7, 14 and 28 days
+current_date <- date_user_table %>%
+  summarise(max_date = max(date)) %>%
+  {.$max_date}
 
 core_user_by_window <- date_user_table %>%
   filter(date >= max(date) - 28, user_id %in% end_users) %>% 
@@ -42,7 +53,12 @@ core_user_by_window <- date_user_table %>%
     , core_past_14 = (past14 > 0) & (past28 > 12)
     , core_past_28 = past28 > 12
   ) %>%
-  summarise(sum(core_past_7), sum(core_past_14), sum(core_past_28))
+  summarise(
+    date = current_date
+    , core_users_past_week = sum(core_past_7)
+    , core_users_past_two_weeks = sum(core_past_14)
+    , core_users_past_month = sum(core_past_28)
+  )
    
   
 # Number of "core" users in past 7, 14 and 28 days
@@ -60,7 +76,28 @@ core_user_by_actual_actions <- date_user_table %>%
     , core_past_14 = past14 > 6
     , core_past_28 = past28 > 12
   ) %>%
-  summarise(sum(core_past_7), sum(core_past_14), sum(core_past_28))
+  summarise(
+    date = current_date
+    , core_users_past_week = sum(core_past_7)
+    , core_users_past_two_weeks = sum(core_past_14)
+    , core_users_past_month = sum(core_past_28)
+  )
+
+# Add new row to google sheet
+
+# First, check to make sure that we're not adding a duplicate row
+
+new_row_duplicated <- core_user_metrics_oldgs %>%
+  match_df(core_user_by_actual_actions) %>% 
+  nrow %>%
+  {. > 0}
+  
+if(!new_row_duplicated){
+  gs_add_row(
+    gs_key("1j2aOKj7pxkvkwHgNzq72mFBBF9AgMz5nP4Tny6LzrfM")
+    , input = core_user_by_window
+  ) 
+}
 
 # User states
 
@@ -76,20 +113,37 @@ user_breakdown_by_current_state_data <- user_facts %>%
   mutate(percent_of_users = round(percent_of_users, 3))
 
 # Produce user breakdown plot
+a <- list()
+for (i in seq_len(nrow(user_breakdown_by_current_state_data))) {
+  m <- user_breakdown_by_current_state_data[i, ]
+  a[[i]] <- list(
+    x = m$current_segment
+    , y = m$percent_of_users
+    , text = prettyNum(m$number_of_users, big.mark = ",")
+    , xref = "x"
+    , yref = "y"
+    , showarrow = F
+    , yanchor = "bottom"
+  )
+}
+
+
 user_breakdown_by_current_state_data %>%
   plot_ly(
     type = "bar"
     , x = current_segment
     , y = percent_of_users
     , text = paste(100*percent_of_users, "% (", as.character(number_of_users), " users)", sep = "")
-    , mode = "text"
+    , mode = "markers"
     , hoverinfo = "text"
+    , color = current_segment
   ) %>%
+  layout(showlegend = F, annotations = a) %>%
   bar_chart_layout(
     charttitle = 
       paste(
         "Percent of Users in Each State ("
-        , user_breakdown_by_current_state_data$total_number_of_users[1]
+        , prettyNum(user_breakdown_by_current_state_data$total_number_of_users[1], big.mark = ",")
         , " Total Users)"
         , sep = ""
       ) 

6d19f38b7eeddf6376b220e420894e6e04857fa6
johnchower <johnchower@gmail.com>
2016-08-01 14:19:13 -0600

    Create fn_plot_triangle_progress.r

---
+++ fn_plot_triangle_progress.R
@@ -0,0 +1,21 @@
+# function: plot_triangle_progress
+
+plot_triangle_progress <-
+  function(
+    mpdbd = mode_pct_data_by_date
+    , user_subset = unique(mpdbd$user_id)
+    , days_since_signup = c(1, 7, 28)
+  ){
+    for(i in days_since_signup){
+      mpdbd %>%
+        prepare_modepctdata_for_createconvexdata(user_subset, number_of_days = i, maxdate = Sys.Date()) %>%
+        create_convex_data_v2 %>%
+        plot_triangle_diagram_v2 %>%
+        {
+          title <- 
+            paste("Day", i, sep = " ")
+          return(. + ggtitle(paste(name, "-", title, sep = " ")))
+        } %>%
+        return
+    }
+  }

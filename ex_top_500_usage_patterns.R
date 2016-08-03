d8507155ca13665e9fabdf659e2a59afd27c0916
johnchower <johnchower@gmail.com>
2016-08-03 11:48:05 -0600

    Include triangle diagrams and cohort breakdowns

--- ex_top_500_usage_patterns.R
+++ ex_top_500_usage_patterns.R
@@ -17,6 +17,10 @@ source("fn_create_convex_data.r")
 source("fn_create_convex_data_v2.r")
 source("fn_plot_triangle_diagram_v2.r")
 source("fn_prepare_modepctdata_for_createconvexdata.r")
+source("fn_plot_triangle_progress.r")
+source("fn_cohort_breakdown.r")
+source("fn_bar_chart_layout.r")
+source("fn_chisquare_analysis.r")
 
 layout <- plotly::layout
 
@@ -28,6 +32,43 @@ standard_user_subset <- user_facts %>%
     account_type == "End User"
   ) %>% 
   {.$user_id} 
+
+FamilyLife_users %>% user_facts %>%
+
+top_10 <- top_users_list$top_10
+top_50 <- top_users_list$top_50
+top_100 <- top_users_list$top_100
+top_250 <- top_users_list$top_250
+top_500 <- top_users_list$top_500
+
+FamilyLife_users <- user_facts %>%
+  filter(champion_id == 6) %>%
+  {.$user_id} %>%
+  unique
+
+cohort_none <- userid_cohortid %>%
+  filter(is.na(cohort_id)) %>%
+  {.$user_id} %>%
+  unique
+
+cohort_students <- userid_cohortid %>%
+  merge(cohortid_cohortname, by = "cohort_id") %>% 
+  merge(cohortid_champid, by = "cohort_id") %>%
+  merge(select(champion_facts, champion_id, champion_name), by = "champion_id") %>%
+  select(user_id, cohort_name, champion_name) %>%
+  dlply(
+    .variables = .(cohort_name, champion_name)
+    , .fun = function(df)unique(df$user_id)
+  )
+
+cohort_none_by_champ <- user_facts %>%
+  filter(user_id %in% cohort_none) %>%
+  merge(select(champion_facts, champion_name, champion_id), by = "champion_id") %>%
+  dlply(
+    .variables = .(champion_name)
+    , .fun = function(df)unique(df$user_id)
+  )
+
 
 # Prepare data for triangle plot ####
 
@@ -63,7 +104,7 @@ mode_pct_data_by_date <- user_platformaction_date_time_mode_timesincecreation %>
 
 rm(user_table)
 
-# Plot activity of each tier through time.
+# Plot activity of each tier through time. ####
 
 top_users_list %>%
   names %>%
@@ -88,10 +129,138 @@ top_users_list %>%
     }
   )
 
-# Take the top cohorts from each tier. Plot those cohorts through time.
+# Take the top cohorts from each tier. Plot those cohorts through time. ####
 
+u.subset <- intersect(top_500, cohort_students$`Growth Facilitator Training Phoenix June 13 @ Salvation Army.Family Bridges`)
+mode_pct_data_by_date %>% 
+  plot_triangle_progress(
+    user_subset = u.subset
+    , subsetname = "Growth Facilitator Training Phoenix June 13, Top 500"
+    , days_since_signup = c(1, 7*(1:6))
+    , save_plots = F
+  ) 
 
+# Cohort breakdowns ####
 
+cohort_breakdown_list <- top_users_list %>%
+  llply(
+    .fun = cohort_breakdown
+  )
 
+# Histogram of cohort breakdowns by tier ####
 
+cohort_summary_list <- cohort_breakdown_list %>%
+  names %>%
+  lapply(
+    FUN = function(name){
+      chartname <- gsub("_", " ", name)
+      df <- cohort_breakdown_list[[name]]
+      user_count <- df %>%
+        {.$user_id} %>%
+        unique %>%
+        length
+      
+      summary_df <- df %>%
+        group_by(description) %>%
+        summarise(number_of_users = length(unique(user_id))) %>% 
+        arrange(desc(number_of_users)) %>%
+        filter(
+          description %in%
+            c(
+              "No cohort - FamilyLife"
+              , "Cohort - CFP"
+              , "No cohort - Gloo"
+              , "Cohort - Cru"
+              , "Cohort - Family Bridges"
+            )
+        )
+      
+      summary_df %>%
+        plot_ly(x = description, y = number_of_users, type = "bar") %>%
+        bar_chart_layout(
+          charttitle = paste("Cohort Breakdown for", chartname, "Users", sep = " ")
+          , bottommargin = 250
+          , yaxistitle = "Number of Users"
+        ) %>%
+        save_or_print(
+          save_plots = F
+        )
+      
+      summary_df %>%
+        {.$description} %>%
+        return
+    }
+  )
+
+# Triangle diagrams of each cohort
+
+# Plot triangle diagrams for each (user tier, cohort) combination. ####
+
+for(i in 1:length(user_cutoffs)){
+  important_cohorts <- cohort_summary_list[[i]]
+  user_subset_0 <- top_users_list[[i]]
+  cohort_breakdown <- cohort_breakdown_list[[i]]
+  tier_size <- user_cutoffs[i]
+  
+  for(cohort in important_cohorts){
+    top_user_cohort_set <- cohort_breakdown %>%
+      filter(description == cohort) %>%
+      {.$user_id} %>%
+      unique
+    
+    mode_pct_data_by_date %>% 
+      plot_triangle_progress(
+        user_subset = top_user_cohort_set
+        , subsetname = paste("Users from top", tier_size, "who belong to", cohort, sep = " ")
+        , days_since_signup = 28
+        , save_plots = F
+      ) 
+  }
+}
+
+# Cluster CFP cohorts
+library(chron)
+
+cohort_untitled_invitation_facts <- 
+  read.csv("cohort_untitled_invitation_facts.csv", stringsAsFactors = F) %>%
+  mutate(
+    createddate = substr(created_at, 1, 10)
+    , createdtime = substr(created_at, 11, 19)
+    , created_date_time = chron(createddate, createdtime, format = c(dates = "y-m-d", times = "h:m:s"))
+  ) 
+
+for(j in c(14, 32)){
+  if(j == 14){project <- "Christian"} else {project <- "Civic"}
+  
+  CFP_cohort_cluster <- cohort_untitled_invitation_facts %>%
+    filter(champion_id == j) %>% # Christian = 14, Civic = 32
+    arrange(created_date_time) 
+  
+  
+  for(i in 1:nrow(CFP_cohort_cluster)){
+    
+    cohort <- CFP_cohort_cluster %>%
+      slice(i) %>%
+      {.$id}
+    
+    user_subset <- cohort %>%
+      {filter(userid_cohortid, cohort_id %in% .)} %>%
+      {.$user_id} %>%
+      unique %>%
+      intersect(top_users_list$top_500)
+    
+    if(length(user_subset) > 10){
+      mode_pct_data_by_date %>% 
+        plot_triangle_progress(
+          user_subset = user_subset
+          , subsetname = paste(project, "CFP", "cohort", cohort, sep = " ")
+          , days_since_signup = 28
+          , save_plots = F
+        )
+    }
+     
+  }
+}
+
+# Chisquare analysis of platform actions taken by cohorts, etc. ####
 

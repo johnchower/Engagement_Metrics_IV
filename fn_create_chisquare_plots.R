b1ec14263443599409abed4ef750eb563b1691db
johnchower <johnchower@gmail.com>
2016-08-04 11:55:18 -0600

    Change the x variable and remove the save_or_print call

--- fn_create_chisquare_plots.R
+++ fn_create_chisquare_plots.R
@@ -7,7 +7,7 @@ create_chisquare_plots <- function(
   , thing_being_counted = "Platform Actions" # What quantity are the bars measuring?
   , thing_being_sliced = "Users" # What does each graph's subset consist of?
   , err_bar_width = 5
-  , args.save_or_print = list() # Pass a list of arguments for the save_or_print function
+#  , args.save_or_print = list() # Pass a list of arguments for the save_or_print function
   , ... # Pass the bar chart layout arguments 
 ){
   plot.data %>%
@@ -18,7 +18,7 @@ create_chisquare_plots <- function(
         out_plot <-  
           plot_ly(
             plot_data
-            , x = plot_data[,1]
+            , x = plot_data[,2]
             , y = percent_expected
             , text = 
               paste("(", prettyNum(round(expected), big.mark = ","), " ", thing_being_counted, " expected)", sep = "")
@@ -27,7 +27,7 @@ create_chisquare_plots <- function(
             , error_y = list(array = err, width = err_bar_width)
           ) %>%
             add_trace(
-              x = plot_data[,1]
+              x = plot_data[,2]
               , y = plot_data$percent_observed
               , text = 
                 paste("(", prettyNum(round(plot_data$observed), big.mark = ","), " ", thing_being_counted, " observed)", sep = "")
@@ -47,8 +47,8 @@ create_chisquare_plots <- function(
               , ...
             )
         
-        do.call(save_or_print, c(out_plot, args.save_or_print)) %>%
-          return
+        #do.call(save_or_print, c(out_plot, args.save_or_print)) %>%
+        #  return
       }
     )
 }

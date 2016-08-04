a4339f5ea14e0d7b37ccdefb51f4be1277edbde9
johnchower <johnchower@gmail.com>
2016-08-04 10:26:44 -0600

    Rename fn_chisquare_plot.r to fn_produce_chisquare_plot_data.r and fix slice_variable default.

---
+++ fn_produce_chisquare_plot_data.r
@@ -0,0 +1,36 @@
+# Function: chisquare_plot
+
+# Takes the result of a chisquare_analysis call and plots the bar charts that
+# compare the expected to the actual counts in each category.
+
+produce_chisquare_plot_data <- 
+  function(
+    results # Take this from a chisquare analysis. You'll have to apply your own filters before calling the function
+    , slice_variable = colnames(results)[1] # Produce one graph for each unique value of slice_variable
+    ){
+    
+    plotdata <- results %>%
+      dlply(
+        .variables = slice_variable
+        , .fun = function(df){
+          dots <- c(colnames(df)[2], "observed", "expected", "err")
+          
+          df %>%
+            arrange(desc(marginal2)) %>%
+            {
+              colnames(.)[3] <- "observed"
+              return(.)
+            } %>%
+            select_(.dots = dots) %>%
+            mutate(
+              total_actions = sum(observed)
+              , percent_observed = observed/sum(observed)
+              , percent_expected = expected/sum(expected)
+              , err = err/total_actions
+            ) %>%
+            return
+        }
+      ) 
+    
+  
+}
